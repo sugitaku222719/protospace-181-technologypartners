fda8e93090abc6ae52b4923958dc6e1762222dac
sugitaku222719 <sugitaku222719@gmail.com>
2024-10-11 14:59:56 +0900

    controller編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    comment = Prototype.create(comment_params)
+    redirect_to prototype_path(comment.prototype.id)
+  end
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
 end
